9898aaa13a7950a8df67fe307a8b86b89d4b4600
Mohamed209 <mohamedmosad209@gmail.com>
2018-12-22 20:27:14 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# multi_open_pose
---
+++ untitled0.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 10 13:42:59 2018
+
+@author: azka
+"""
+
+#ffmpeg -i movie.mp4 -ss 00:00:03 -t 00:00:08 -async 1 cut.mp4
